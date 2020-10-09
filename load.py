77e075acb9b8bc7f82673be00a0bb420695933f3
k-pradeep <36804772+k-pradeep@users.noreply.github.com>
2020-10-08 21:20:19 -0400

    S3 commit
    Add code to store the data in S3

--- load.py
+++ load.py
@@ -33,8 +33,9 @@ def sns_setup():
         topic_arn = topic_response['TopicArn']
         print(f'Topic Arn is {topic_arn}')
         if topic_response['ResponseMetadata']['HTTPStatusCode'] == 200:
-            #subscribe to topic
-            subscribe_response = sns.subscribe(TopicArn=topic_arn, Protocol='sms', Endpoint='+16476123784')
+            #subscribe to topic, Enter phone number
+            phonenumber =''
+            subscribe_response = sns.subscribe(TopicArn=topic_arn, Protocol='sms', Endpoint=phonenumber)
             print(f'Subscription response is {subscribe_response}')
             # Send a SMS message to the specified phone number
             publish_response = sns.publish(
@@ -44,7 +45,7 @@ def sns_setup():
             print(f'Publish response is {publish_response}')
 
 
-def send_notifiocation(message_body):
+def send_notification(message_body):
     print('Enterning send Notification')
     message = message_body
     sns = boto3.client('sns')
@@ -192,6 +193,28 @@ def load_table(data):
     print(f'Response from run date insert is {response_rundate}')
 
 
+def copy_to_s3_csv(bucket_name):
+
+    # insert data into csv file for dashboard
+    s3_client = boto3.client('s3')
+    covid_data = get_item(table_name='covid_data')
+    print('****************************************')
+    print(covid_data)
+    data_csv_format = []
+    for data_item in covid_data:
+        data_csv_format.append(
+        f"{data_item['recorded_date']},{data_item['recoveries']},{data_item['cases']},{data_item['deaths']}")
+        # temp file name
+        file_path = '/tmp/' + 'covid_data.csv'
+        with open(file_path, 'w') as f:
+            f.write('recorded_date,recoveries,cases,deaths \n')
+            for data_item in data_csv_format:
+                f.write(data_item + '\n')
+        # copy the file to s3
+        s3_response = s3_client.upload_file(file_path, bucket_name, 'covid_data.csv')
+        print(s3_response)
+
+
 def get_item(table_name):
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table(table_name)
@@ -213,23 +236,26 @@ def get_item(table_name):
         return response['Items']
 
 
+
 #if __name__== '__main__' :
 def lambda_handler(event, context):
     try:
-
         dynamodb = boto3.client('dynamodb')
         #table name
         table_name = 'covid_data'
         existing_tables = dynamodb.list_tables()['TableNames']
         print(existing_tables)
         full_load = False
+        us_url ="https://raw.githubusercontent.com/nytimes/covid-19-data/master/us.csv"
+        john_hopkins_data_url = "https://raw.githubusercontent.com/datasets/covid-19/master/data/time-series-19-covid-combined.csv?opt_id=oeu1597116776410r0.9370121973993177"
         if table_name not in existing_tables:
             create_table(table_name)
             full_load = True
 
 
         if full_load:
-            data = extract()
+
+            data = extract(us_url,john_hopkins_data_url)
             print(f'data type of extract data is {type(data)}')
             list_data = []
             for item in iter(data.values):
@@ -240,9 +266,6 @@ def lambda_handler(event, context):
 
             #Initial load to DB
             load_table(list_data)
-
-            # Success Notification
-            send_notifiocation('ETL process completed successfully')
         else:
             # get last run date from rundate table
             rundate_data = get_item('rundate')
@@ -253,7 +276,7 @@ def lambda_handler(event, context):
                 last_row_count = rundate_data[0]['row_count']
                 print(f'Rundate is {last_run_date}, row count is {last_row_count}')
             #Get Data from transform
-            data_df = extract()
+            data_df = extract(us_url,john_hopkins_data_url)
             if not last_run_date == '':
                 data_df = data_df[(data_df['date']> last_run_date)]
             print(f'data type of extract data is {type(data_df)}')
@@ -264,36 +287,32 @@ def lambda_handler(event, context):
                 list_data.append(item)
                 print('*****************')
                 print(list_data)
-
             load_table(list_data)
-
-            #Success Notification
-            send_notifiocation('ETL process completed successfully')
-
-
-            # response = get_item()
-            #
-            # #logic to retrieve last run date based on retrieving all rows from database
-            # last_run_date = datetime.datetime.strptime('1900-01-01', '%Y-%m-%d').date()
-            # for item in response:
-            #     recorded_date_db = item['recorded_date']
-            #     recorded_datetime = datetime.datetime.strptime(recorded_date_db,'%Y-%m-%d')
-            #     #print(recorded_datetime.date())
-            #     if (last_run_date < recorded_datetime.date()):
-            #          last_run_date = recorded_datetime.date()
-            # print(last_run_date)
-                # recorded_date = item['recorded_date']
-                # print(recorded_date)
-             #print(f'Data from DB is {response}')
-            return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
+        #insert data into s3 for building dashboards
+        s3_client = boto3.client('s3')
+        bucket_list = s3_client.list_buckets()
+        bucket_name = 'python-code-acloudguru-challenge'
+        bucket_list = bucket_list['Buckets']
+        print(bucket_list)
+        bucket_name_list = []
+        if len(bucket_list) > 0:
+            for item in bucket_list:
+                bucket_name_list.append(item['Name'])
+                # item['Name'])
+            if bucket_name in bucket_name_list:
+                print(f'{bucket_name} is available')
+                copy_to_s3_csv(bucket_name)
+            else:
+                response = None
+                response = s3_client.create_bucket(Bucket=bucket_name)
+                print(f'{response}')
+                copy_to_s3_csv(bucket_name)
+            print(bucket_name_list)
+        # Success Notification
+        send_notification('ETL process completed successfully')
+        return "Event Driven ETL Processed succesfully"
     except Exception as e:
         print('In exception block')
         print(e)
-        send_notifiocation('ETL failed. Please take a look')
-        return {
-            'statusCode': 500,
-            'body': json.dumps('Hello from Lambda!')
-        }
+        send_notification('ETL failed. Please take a look')
+        #return "Event Driven ETL Processed Failed"
